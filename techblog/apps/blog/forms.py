d0e9c7912b3960749511eccd01e80c95f2dde7c4
willmcgugan <willmcgugan@6cc07630-a813-11dd-a8c6-677c70d10fe4>
2014-01-30 22:05:17 +0000

    fix

--- techblog/apps/blog/forms.py
+++ techblog/apps/blog/forms.py
@@ -3,18 +3,16 @@ from django import forms
 
 class ImportForm(forms.Form):
 
-    blog_slug = forms.CharField("Blog slug")
+    blog_slug = forms.CharField(label="Blog slug")
     input_file = forms.FileField()
+    include_tags = forms.CharField(label="Included tags", required=False)
+    exclude_tags = forms.CharField(label="Excluded tags", required=False)
+    format = forms.CharField(label="Import format", widget=forms.HiddenInput, initial="WXR")
 
-    include_tags = forms.CharField("Included tags", required=False)
-    exclude_tags = forms.CharField("Excluded tags", required=False)
-
-    format = forms.CharField("Import format", widget=forms.HiddenInput, initial="WXR")
 
 class WriterForm(forms.Form):
 
     title = forms.CharField(label="Post Title", required=True)
     slug = forms.CharField(label="Post Slug", required=True)
     tags = forms.CharField(label="Tags (comma separated)", required=True)
-
     post = forms.CharField(label="Post Content", required=False)
