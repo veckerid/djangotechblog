d72600b111f01539dc87c504c05ee28b124299fc
willmcgugan <willmcgugan@6cc07630-a813-11dd-a8c6-677c70d10fe4>
2014-01-30 23:06:35 +0000

    fixes

--- techblog/manage.py
+++ techblog/manage.py
@@ -2,6 +2,9 @@
 import os
 import sys
 
+project_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(project_dir))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techblog.settings")
 
